13e35daf9b45c2ca39300e3b12465eb70d005096
bukebusek <779742678@qq.com>
2021-03-23 23:20:57 +0800

    Day9 Update

--- Day9/SalarySettlement.py
+++ Day9/SalarySettlement.py
@@ -35,3 +35,87 @@ for循环员工列表
 判断是否是销售员类 如果是 则输入销售额
 打印本月工资
 """
+
+from abc import ABCMeta, abstractmethod
+
+class Employee(object, metaclass=ABCMeta):
+    """员工"""
+    def __init__(self, name):
+        """
+        初始化方法
+        :param name:姓名 
+        """
+        self._name = name 
+        
+    @property
+    def name(self):
+        return self._name
+    
+    @abstractmethod
+    def get_salary(self):
+        """
+        获得月薪
+        :return:月薪 
+        """
+        pass
+    
+class Manager(Employee):
+    """部门经理"""
+    def __init__(self, name):
+        super().__init__(name)
+    
+    def get_salary(self):
+        return 15000.00
+    
+class Programmer(Employee):
+    """程序员"""
+    def __init__(self, name, working_hour=0):
+        super(Programmer, self).__init__(name)
+        self._working_hour = working_hour
+    
+    @property
+    def working_hour(self):
+        return  self._working_hour
+    
+    @working_hour.setter
+    def working_hour(self, working_hour):
+        self._working_hour = working_hour if working_hour > 0 else 0
+    
+    def get_salary(self):
+        return 150.00 * self._working_hour
+    
+class Salesman(Employee):
+    """销售员"""
+    
+    def __init__(self, name, sales=0):
+        super(Salesman, self).__init__(name)
+        self._sales = sales
+
+    @property
+    def sales(self):
+        return self._sales
+
+    @sales.setter
+    def sales(self, sales):
+        self._sales = sales if sales > 0 else 0
+
+    def get_salary(self):
+        return 1200.00 + self._sales * 0.05
+
+def main():
+    emps = [
+        Manager('刘备'), Programmer('诸葛亮', 80),
+        Manager('曹操'), Salesman('荀彧', 20000),
+        Salesman('吕布'), Programmer('张辽', 120),
+        Programmer('赵云', 50)
+    ]
+    for emp in emps:
+        #if isinstance(emp, Programmer):
+            #emp.working_hour = int(input('请输入%s本月的工作时间' % emp.name))
+        #elif isinstance(emp, Salesman):
+            #emp.sales = float(input('请输入%s本月的销售额' % emp.name))
+        print('%s本月工资为：￥%s元' % (emp.name, emp.get_salary()))
+
+if __name__ == '__main__':
+    main()
+
