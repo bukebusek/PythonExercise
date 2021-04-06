40ddf468f2a8c7d53ec02e9656238eeb788f8d77
bukebusek <779742678@qq.com>
2021-04-07 03:06:09 +0800

    Day13 Update

---
+++ Day13 Processes and threads/ExercisePT.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+# @File    : ExercisePT.py
+# @Author  : Xukun
+# @Time    : 2021/4/7 02:00
+
+from random import randint
+from threading import Thread, Lock
+from time import time, sleep
+
+
+class DownloadTask(Thread):
+
+    def __init__(self, filename):
+        super().__init__()
+        self._filename = filename
+
+    def run(self):
+        print('开始下载%s...' % self._filename)
+        time_to_download = randint(5, 10)
+        sleep(time_to_download)
+        print('%s下载完成! 耗费了%d秒' % (self._filename, time_to_download))
+
+
+def main():
+    start = time()
+    t1 = DownloadTask('Python从入门到住院.pdf')
+    t1.start()
+    t2 = DownloadTask('Peking Hot.avi')
+    t2.start()
+    t1.join()
+    t2.join()
+    end = time()
+    print('总共耗费了%.2f秒.' % (end - start))
+
+class Account(object):
+
+    def __init__(self) -> object:
+        self._balance = 0
+        self._lock = Lock()
+
+    def deposit(self, money):
+        # 先获取锁才能执行后续代码
+        self._lock.acquire()
+        try:
+            new_balance = self._balance + money
+            # 模拟受理存款业务需要0.01秒的时间
+            sleep(0.01)
+            # 修改账户余额
+            self._balance = new_balance
+        finally:
+            # 在finally中执行释放锁的操作保证正常异常锁都能释放
+            self._lock.release()
+
+    @property
+    def balance(self):
+        return self._balance
+
+
+class AddMoneyThread(Thread):
+
+    def __init__(self, account, money):
+        super().__init__()
+        self._account = account
+        self._money = money
+
+    def run(self):
+        self._account.deposit(self._money)
+
+
+def main2():
+    account = Account()
+    threads = []
+    # 创建100个存款的线程向同一个账户中存钱
+    for _ in range(100):
+        t = AddMoneyThread(account, 1)
+        threads.append(t)
+        t.start()
+    # 等所有存款的线程都执行完毕
+    for t in threads:
+        t.join()
+    print('账户余额为: ￥%d元' % account.balance)
+
+if __name__ == '__main__':
+    main2()
